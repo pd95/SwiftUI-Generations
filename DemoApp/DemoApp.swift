335858725ff5c54d3980b75b2f253bc05e1b7248
Philipp <pd95@users.noreply.github.com>
2022-02-13 13:29:17 +0100

    Move `AppDelegate` required for iOS 13 to `DemoApp` explaining the different ways to setup the initial scene.

--- DemoApp/DemoApp.swift
+++ DemoApp/DemoApp.swift
@@ -7,7 +7,22 @@
 
 import SwiftUIShim
 
-#if !TARGET_IOS_MAJOR_13
+#if TARGET_IOS_MAJOR_13
+
+/// iOS 13 uses the `AppDelegate` and `SceneDelegate` protocol to setup the initial scene
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        print(#function)
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
+
+#else
+
+// iOS 14 and above are based on the `App` protocol and the `@main` annotation to setup the initial scene
 @main
 struct DemoApp: App {
     var body: some Scene {
@@ -16,4 +31,5 @@ struct DemoApp: App {
         }
     }
 }
+
 #endif
