24ac78714c1df7db9d59da059537c40dcfa79911
Philipp <pd95@users.noreply.github.com>
2022-05-30 17:09:24 +0200

    Adding DemoApp for macOS 11: Xcode build target and base sources without any functionality yet!

---
+++ DemoApp/macOS/DemoAppMac.swift
@@ -0,0 +1,20 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Philipp on 30.05.22.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello World!")
+                .font(.largeTitle)
+                .padding()
+                .fixedSize()
+        }
+    }
+}
